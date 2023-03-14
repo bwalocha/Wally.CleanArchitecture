c5f22608f1225ee0a4c587c5d5e222550c53ef3e
Bartlomiej.Walocha@qiagen.com <Bartlomiej.Walocha@contractor.qiagen.com>
2023-03-14 14:41:21 +0100

    DomainEvent tests

--- src/Wally.CleanArchitecture.MicroService.ConventionTests/DomainEventTests.cs
+++ src/Wally.CleanArchitecture.MicroService.ConventionTests/DomainEventTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 using FluentAssertions;
 using FluentAssertions.Execution;
@@ -31,14 +32,14 @@ public class DomainEventTests
 	}
 
 	[Fact]
-	public void Domain_AllClassesEndsWithEvent_ShouldInheritDomainEvent()
+	public void Domain_AllClassesEndsWithDomainEvent_ShouldInheritDomainEvent()
 	{
-		var assemblies = Configuration.Assemblies.GetAllAssemblies();
+		var assemblies = Configuration.Assemblies.Domain;
 		var types = assemblies.GetAllTypes();
 
 		using (new AssertionScope())
 		{
-			foreach (var type in types.Where(a => a.Name.EndsWith("Event"))
+			foreach (var type in types.Where(a => a.Name.EndsWith("DomainEvent"))
 						.Where(a => a != typeof(DomainEvent)))
 			{
 				type.Should()
@@ -48,9 +49,9 @@ public class DomainEventTests
 	}
 
 	[Fact]
-	public void Domain_AllClassesWhichInheritsDomainEvent_ShouldHasEventSuffix()
+	public void Domain_AllClassesWhichInheritsDomainEvent_ShouldHaveDomainEventSuffix()
 	{
-		var assemblies = Configuration.Assemblies.GetAllAssemblies();
+		var assemblies = Configuration.Assemblies.Domain;
 		var types = assemblies.GetAllTypes();
 
 		using (new AssertionScope(new AssertionStrategy()))
@@ -58,7 +59,7 @@ public class DomainEventTests
 			foreach (var type in types.ThatImplement<DomainEvent>())
 			{
 				type.Name.Should()
-					.EndWith("Event");
+					.EndWith("DomainEvent");
 			}
 		}
 	}
@@ -66,7 +67,7 @@ public class DomainEventTests
 	[Fact]
 	public void Domain_DomainEvent_ShouldNotExposeSetter()
 	{
-		var assemblies = Configuration.Assemblies.GetAllAssemblies();
+		var assemblies = Configuration.Assemblies.Domain;
 
 		using (new AssertionScope(new AssertionStrategy()))
 		{
@@ -78,6 +79,27 @@ public class DomainEventTests
 				types.Properties()
 					.Should()
 					.NotBeWritable("Request should be immutable");
+			}
+		}
+	}
+	
+	[Fact]
+	public void Domain_DomainEvent_ShouldBeExcludedFromCodeCoverage()
+	{
+		var assemblies = Configuration.Assemblies.Domain;
+		
+		using (new AssertionScope(new AssertionStrategy()))
+		{
+			foreach (var assembly in assemblies)
+			{
+				var types = assembly.Types()
+					.ThatImplement<DomainEvent>();
+
+				foreach (var type in types)
+				{
+					type.Should()
+						.BeDecoratedWith<ExcludeFromCodeCoverageAttribute>();
+				}
 			}
 		}
 	}
