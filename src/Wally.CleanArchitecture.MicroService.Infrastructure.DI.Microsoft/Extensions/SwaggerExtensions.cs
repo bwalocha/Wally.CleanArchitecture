9e9bebd368fab7a74e986f2917ec23d6752b775b
Oskar <oskar@infomentor.co.uk>
2023-05-22 17:00:46 +0200

    Split swagger operation filters to separate files

--- src/Wally.CleanArchitecture.MicroService.Infrastructure.DI.Microsoft/Extensions/SwaggerExtensions.cs
+++ src/Wally.CleanArchitecture.MicroService.Infrastructure.DI.Microsoft/Extensions/SwaggerExtensions.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.OData.Query;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 
-using Swashbuckle.AspNetCore.SwaggerGen;
-
 using Wally.CleanArchitecture.MicroService.Infrastructure.DI.Microsoft.Models;
+using Wally.CleanArchitecture.MicroService.Infrastructure.DI.Microsoft.Swagger;
 
 namespace Wally.CleanArchitecture.MicroService.Infrastructure.DI.Microsoft.Extensions;
 
@@ -45,6 +42,7 @@ public static class SwaggerExtensions
 				var xmlFilename = $"{assembly.GetName().Name}.xml";
 				options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 
+				options.OperationFilter<ResponseTypesOperationFilter>();
 				options.OperationFilter<ODataQueryOptionsOperationFilter>();
 			});
 
@@ -64,81 +62,5 @@ public static class SwaggerExtensions
 			});
 
 		return app;
-	}
-
-	private class ODataQueryOptionsOperationFilter : IOperationFilter
-	{
-		public void Apply(OpenApiOperation operation, OperationFilterContext context)
-		{
-			var odataQueryParameterTypes = context.ApiDescription.ActionDescriptor.Parameters
-				.Where(p => p.ParameterType.IsAssignableTo(typeof(ODataQueryOptions)))
-				.ToList();
-
-			if (!odataQueryParameterTypes.Any())
-			{
-				return;
-			}
-
-			// Remove the large queryOptions field from Swagger which gets added by default...
-			foreach (var queryParamType in odataQueryParameterTypes)
-			{
-				var paramToRemove = operation.Parameters.SingleOrDefault(p => p.Name == queryParamType.Name);
-				if (paramToRemove != null)
-				{
-					operation.Parameters.Remove(paramToRemove);
-				}
-			}
-
-			// ...and add our own query parameters.
-			operation.Parameters.Add(
-				new OpenApiParameter
-				{
-					Name = "$filter",
-					In = ParameterLocation.Query,
-					Description = "Filter the results",
-					Required = false,
-					Schema = new OpenApiSchema { Type = "string", },
-				});
-
-			operation.Parameters.Add(
-				new OpenApiParameter
-				{
-					Name = "$orderby",
-					In = ParameterLocation.Query,
-					Description = "Order the results",
-					Required = false,
-					Schema = new OpenApiSchema { Type = "string", },
-				});
-
-			operation.Parameters.Add(
-				new OpenApiParameter
-				{
-					Name = "$select",
-					In = ParameterLocation.Query,
-					Description = "Select the properties to be returned in the response",
-					Required = false,
-					Schema = new OpenApiSchema { Type = "string", },
-				});
-
-			operation.Parameters.Add(
-				new OpenApiParameter
-				{
-					Name = "$top",
-					In = ParameterLocation.Query,
-					Description = "Limit the number of results returned",
-					Required = false,
-					Schema = new OpenApiSchema { Type = "integer", Format = "int32", },
-				});
-
-			operation.Parameters.Add(
-				new OpenApiParameter
-				{
-					Name = "$skip",
-					In = ParameterLocation.Query,
-					Description = "Skip the specified number of results",
-					Required = false,
-					Schema = new OpenApiSchema { Type = "integer", Format = "int32", },
-				});
-		}
 	}
 }
---
+++ src/Wally.CleanArchitecture.MicroService.Infrastructure.DI.Microsoft/Swagger/ResponseTypesOperationFilter.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Wally.CleanArchitecture.MicroService.Infrastructure.DI.Microsoft.Swagger;
+
+internal class ResponseTypesOperationFilter : IOperationFilter
+{
+	/// <summary>
+	///	 Applies the filter to the specified operation using the given context.
+	/// </summary>
+	/// <param name="operation">The operation to apply the filter to.</param>
+	/// <param name="context">The current operation filter context.</param>
+	public void Apply(OpenApiOperation operation, OperationFilterContext context)
+	{
+		// operation.Deprecated |= apiDescription.IsDeprecated();
+
+		// REF: https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/1752#issue-663991077
+		foreach (var responseType in context.ApiDescription.SupportedResponseTypes)
+		{
+			// REF: https://github.com/domaindrivendev/Swashbuckle.AspNetCore/blob/b7cf75e7905050305b115dd96640ddd6e74c7ac9/src/Swashbuckle.AspNetCore.SwaggerGen/SwaggerGenerator/SwaggerGenerator.cs#L383-L387
+			var responseKey = responseType.IsDefaultResponse ? "default" : responseType.StatusCode.ToString();
+			var response = operation.Responses[responseKey];
+
+			foreach (var contentType in response.Content.Keys)
+			{
+				if (!responseType.ApiResponseFormats.Any(x => x.MediaType == contentType))
+				{
+					response.Content.Remove(contentType);
+				}
+			}
+		}
+	}
+}
