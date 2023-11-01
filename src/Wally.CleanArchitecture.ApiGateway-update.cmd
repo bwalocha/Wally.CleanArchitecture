17af0d3174cffbf8dc72e1feb12c9d40d79113d3
wally <b.walocha@gmail.com>
2023-11-01 21:49:29 +0100

    up [skip ci]

--- src/Wally.CleanArchitecture.ApiGateway-update.cmd
+++ src/Wally.CleanArchitecture.ApiGateway-update.cmd
@@ -1,4 +1,8 @@
 SET "NAME=Wally.CleanArchitecture"
-SET "SERVICE_NAME=ApiGatevay"
-git diff --exit-code && dotnet new wally.cleanarchitecture --output . --name %NAME% --serviceName %SERVICE_NAME% -proxy=true --force
+SET "SERVICE_NAME=ApiGateway"
+
+SET "AUTHOR_NAME=wally"
+SET "TEMPLATE_NAME=%AUTHOR_NAME%.cleanarchitecture"
+
+git diff --exit-code && dotnet new %TEMPLATE_NAME% --output . --name %NAME% --serviceName %SERVICE_NAME% -proxy=true --force
 pause
--- src/Wally.CleanArchitecture.MicroService-update.cmd
+++ src/Wally.CleanArchitecture.MicroService-update.cmd
@@ -1,4 +1,8 @@
 SET "NAME=Wally.CleanArchitecture"
 SET "SERVICE_NAME=MicroService"
-git diff --exit-code && dotnet new wally.cleanarchitecture --output . --name %NAME% --serviceName %SERVICE_NAME% -service=true --force
+
+SET "AUTHOR_NAME=wally"
+SET "TEMPLATE_NAME=%AUTHOR_NAME%.cleanarchitecture"
+
+git diff --exit-code && dotnet new %TEMPLATE_NAME% --output . --name %NAME% --serviceName %SERVICE_NAME% -service=true --force
 pause
