dbd390d53edfd3d1bf1bfe025b3229fd9a9a0edf
wally <b.walocha@gmail.com>
2024-05-21 21:09:49 +0200

    up

--- src/Wally.CleanArchitecture.ApiGateway-update.cmd
+++ src/Wally.CleanArchitecture.ApiGateway-update.cmd
@@ -4,6 +4,6 @@ SET "SERVICE_NAME=ApiGateway"
 SET "AUTHOR_NAME=wally"
 SET "TEMPLATE_NAME=%AUTHOR_NAME%.cleanarchitecture"
 
-dotnet new install %TEMPLATE_NAME%
+dotnet new install %TEMPLATE_NAME%.Template
 git diff --exit-code && dotnet new %TEMPLATE_NAME% --output . --name %NAME% --serviceName %SERVICE_NAME% -proxy=true --force
 pause
--- src/Wally.CleanArchitecture.MicroService-update.cmd
+++ src/Wally.CleanArchitecture.MicroService-update.cmd
@@ -4,6 +4,6 @@ SET "SERVICE_NAME=MicroService"
 SET "AUTHOR_NAME=wally"
 SET "TEMPLATE_NAME=%AUTHOR_NAME%.cleanarchitecture"
 
-dotnet new install %TEMPLATE_NAME%
+dotnet new install %TEMPLATE_NAME%.Template
 git diff --exit-code && dotnet new %TEMPLATE_NAME% --output . --name %NAME% --serviceName %SERVICE_NAME% -service=true --force
 pause
