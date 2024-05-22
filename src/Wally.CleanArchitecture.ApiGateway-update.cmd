35ebb7a34419d4a9da1fbc1d79d82e259af26f9d
wally <b.walocha@gmail.com>
2024-05-22 21:00:36 +0200

    up

--- src/Wally.CleanArchitecture.ApiGateway-update.cmd
+++ src/Wally.CleanArchitecture.ApiGateway-update.cmd
@@ -1,8 +1,8 @@
 SET "NAME=Wally.CleanArchitecture"
 SET "SERVICE_NAME=ApiGateway"
 
-SET "AUTHOR_NAME=wally"
-SET "TEMPLATE_NAME=%AUTHOR_NAME%.cleanarchitecture"
+SET "AUTHOR_NAME=Wally"
+SET "TEMPLATE_NAME=%AUTHOR_NAME%.CleanArchitecture"
 
 dotnet new install %TEMPLATE_NAME%.Template
 git diff --exit-code && dotnet new %TEMPLATE_NAME% --output . --name %NAME% --serviceName %SERVICE_NAME% -proxy=true --force
