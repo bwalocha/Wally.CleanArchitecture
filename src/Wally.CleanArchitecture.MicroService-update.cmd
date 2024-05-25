b773bb4c4bd3f2ac27e2e0c49a0bc2791b486086
wally <b.walocha@gmail.com>
2024-05-25 19:17:18 +0200

    up

--- src/Wally.CleanArchitecture.MicroService-update.cmd
+++ src/Wally.CleanArchitecture.MicroService-update.cmd
@@ -7,5 +7,5 @@ SET "TEMPLATE_NAME=%AUTHOR_NAME%.CleanArchitecture"
 SET "TEMPLATE_SHORT_NAME=%AUTHOR_SHORT_NAME%.cleanarchitecture"
 
 dotnet new install %TEMPLATE_NAME%.Template
-git diff --exit-code && dotnet new %TEMPLATE_SHORT_NAME% --output . --name %NAME% --serviceName %SERVICE_NAME% -proxy=true --force
+git diff --exit-code && dotnet new %TEMPLATE_SHORT_NAME% --output . --name %NAME% --serviceName %SERVICE_NAME% -service=true --force
 pause
