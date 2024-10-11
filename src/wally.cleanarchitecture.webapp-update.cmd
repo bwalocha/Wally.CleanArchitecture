bac35f9b0f30807f8b87c0383a9d61cc191afa46
Wally <b.walocha@gmail.com>
2024-10-11 22:10:25 +0200

    shadcn - NewYork/Neutral [skip ci]

--- src/wally.cleanarchitecture.webapp-update.cmd
+++ src/wally.cleanarchitecture.webapp-update.cmd
@@ -1,10 +1,11 @@
 SET "WEBAPP=wally.cleanarchitecture.webapp"
 
 git diff --exit-code || echo ERROR: Commit your git changes! && exit /B
-rmdir .\%WEBAPP% /S /Q
+rmdir .\%WEBAPP% /S /Q || echo ERROR: Commit your git changes! && exit /B
 call npx create-next-app@latest --typescript --tailwind --eslint --src-dir --app --no-import-alias --use-npm %WEBAPP%
 cd ./%WEBAPP%
 call npm i
+call npx shadcn@latest init --defaults --force
 call npm run build
 cd ..
 pause
