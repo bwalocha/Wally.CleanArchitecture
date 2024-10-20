c1d1fdf8dccea83e9efeedfa2575e5353ad24477
wally <b.walocha@gmail.com>
2024-10-20 10:35:40 +0200

    Sidebar [skip ci]

---
+++ src/wally.cleanarchitecture.webapp/src/components/AppSidebar.tsx
@@ -0,0 +1,174 @@
+﻿import {Calendar, ChevronDown, ChevronUp, Home, Inbox, Plus, Search, Settings, User2} from "lucide-react"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupAction,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuAction,
+    SidebarMenuBadge,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarRail,
+    SidebarSeparator,
+} from "@/components/ui/sidebar"
+import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import {Collapsible, CollapsibleContent, CollapsibleTrigger} from "@/components/ui/collapsible";
+
+// Menu items.
+const items = [
+    {
+        title: "Home",
+        url: "#",
+        icon: Home,
+    },
+    {
+        title: "Inbox",
+        url: "#",
+        icon: Inbox,
+    },
+    {
+        title: "Calendar",
+        url: "#",
+        icon: Calendar,
+    },
+    {
+        title: "Search",
+        url: "#",
+        icon: Search,
+    },
+    {
+        title: "Settings",
+        url: "#",
+        icon: Settings,
+    },
+]
+
+export function AppSidebar() {
+    return (
+        <Sidebar collapsible={"icon"}>
+            <SidebarHeader className="group-data-[collapsible=icon]:hidden">
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <SidebarMenuButton>
+                                    Select Workspace
+                                    <ChevronDown className="ml-auto" />
+                                </SidebarMenuButton>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+                                <DropdownMenuItem>
+                                    <span>Acme Inc</span>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <span>Acme Corp.</span>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+
+            <SidebarSeparator className="group-data-[collapsible=icon]:hidden" />
+            
+            <SidebarContent>
+                <Collapsible defaultOpen className="group/collapsible">
+                    <SidebarGroup className="group-data-[collapsible=icon]:hidden">
+                        <SidebarGroupLabel asChild>
+                            <CollapsibleTrigger>
+                                Help
+                                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                            </CollapsibleTrigger>
+                        </SidebarGroupLabel>
+                        <CollapsibleContent>
+                            <SidebarGroupContent />
+                        </CollapsibleContent>
+                    </SidebarGroup>
+                </Collapsible>
+
+                <SidebarSeparator className="group-data-[collapsible=icon]:hidden" />
+                
+                <SidebarGroup className="group-data-[collapsible=icon]:hidden">
+                    <SidebarGroupLabel asChild>Projects</SidebarGroupLabel>
+                    <SidebarGroupAction title="Add Project">
+                        <Plus /> <span className="sr-only">Add Project</span>
+                    </SidebarGroupAction>
+                    <SidebarGroupContent />
+                </SidebarGroup>
+
+                <SidebarSeparator className="group-data-[collapsible=icon]:hidden" />
+                
+                <SidebarGroup>
+                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+                    <SidebarGroupAction>
+                        <Plus /> <span className="sr-only">Add Project</span>
+                    </SidebarGroupAction>
+                    <SidebarGroupContent></SidebarGroupContent>
+                </SidebarGroup>
+
+                <SidebarSeparator className="group-data-[collapsible=icon]:hidden" />
+                
+                <SidebarGroup>
+                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                    <SidebarMenuBadge>24</SidebarMenuBadge>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+
+                
+            </SidebarContent>
+
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton />
+                        <SidebarMenuAction className="peer-data-[active=true]/menu-button:opacity-100" />
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <SidebarMenuButton>
+                                    <User2 /> Username
+                                    <ChevronUp className="ml-auto" />
+                                </SidebarMenuButton>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent
+                                side="top"
+                                className="w-[--radix-popper-anchor-width]"
+                            >
+                                <DropdownMenuItem>
+                                    <span>Account</span>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <span>Billing</span>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <span>Sign out</span>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
+
+            <SidebarRail />
+        </Sidebar>
+    )
+}
---
+++ src/wally.cleanarchitecture.webapp/src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+﻿"use client"
+
+import Image from "next/image";
+import {Menu} from "lucide-react";
+import {Button} from "@/components/ui/button";
+import { SidebarTrigger } from "@/components/ui/sidebar"
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {ThemeSwitch} from "@/components/ThemeSwitch";
+
+// https://kit.shadcnui.com/components/layout/navbar
+function Navbar() {
+    return (
+        <header>
+            <nav className="flex items-center justify-between bg-neutral-200">
+                <div className="flex">
+                    <Button>
+                        <Menu/>
+                    </Button>
+                    <Image
+                        className="dark:invert"
+                        src="/logo.svg"
+                        alt="logo"
+                        width={180}
+                        height={38}
+                        priority
+                    />
+                </div>
+                <div className="flex">
+                    <ThemeSwitch/>
+                    <Avatar className="mr-2">
+                        <AvatarImage src="https://github.com/shadcn.png"/>
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <SidebarTrigger className="-mr-1 ml-auto rotate-180" />
+                </div>
+            </nav>
+        </header>
+);
+}
+
+export {
+    Navbar
+}
