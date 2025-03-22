a75d548afff964e3e58ddfb2a9f666c2ec58627e
wally <b.walocha@gmail.com>
2025-03-22 16:38:00 +0100

    up

---
+++ src/Wally.CleanArchitecture.MicroService/Wally.CleanArchitecture.MicroService.Infrastructure.Persistence.SqlServer/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Wally.CleanArchitecture.MicroService.Domain.Abstractions;
+
+namespace Wally.CleanArchitecture.MicroService.Infrastructure.Persistence.SqlServer.Extensions;
+
+public static class ModelBuilderExtensions
+{
+	public static ModelBuilder ApplyChangeTracking(this ModelBuilder modelBuilder)
+	{
+		var allEntities = modelBuilder.Model.GetEntityTypes();
+		foreach (var entity in allEntities
+					.Where(a => typeof(ISoftDeletable)
+						.IsAssignableFrom(a.ClrType)) // TODO: use different interface or apply for all entities
+					.Where(a => string.IsNullOrEmpty(a.GetViewName()))
+					.Select(a => a.ClrType)
+					.ToArray())
+		{
+			var entityBuilder = modelBuilder.Entity(entity);
+
+			// https://learn.microsoft.com/en-us/ef/core/providers/sql-server/temporal-tables
+			entityBuilder.ToTable(
+				entity.Name,
+				a => a.IsTemporal(
+					/*b =>
+					{
+						b.HasPeriodStart("ValidFrom");
+						b.HasPeriodEnd("ValidTo");
+						b.UseHistoryTable($"{entity.Name}_HistoricalData");
+					}*/
+				));
+		}
+
+		return modelBuilder;
+	}
+}
