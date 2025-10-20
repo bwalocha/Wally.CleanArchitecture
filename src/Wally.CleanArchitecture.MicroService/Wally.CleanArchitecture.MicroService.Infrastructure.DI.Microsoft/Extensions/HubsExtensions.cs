3b1946ffdb01775088a85a87a604f2bb6c45dd32
wally <b.walocha@gmail.com>
2025-10-20 22:38:27 +0200

    Update packages

--- src/Wally.CleanArchitecture.MicroService/Wally.CleanArchitecture.MicroService.Infrastructure.DI.Microsoft/Extensions/HubsExtensions.cs
+++ src/Wally.CleanArchitecture.MicroService/Wally.CleanArchitecture.MicroService.Infrastructure.DI.Microsoft/Extensions/HubsExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -19,7 +21,24 @@ public static class HubsExtensions
 		app.UseDefaultFiles();
 		app.UseStaticFiles();
 
-		app.UseEndpoints(endpoints => { endpoints.MapHub<THub>("/hub"); });
+		app.UseEndpoints(endpoints =>
+		{
+			// TODO: Use type name
+			endpoints.MapHub<THub>("/hub", options =>
+			{
+				options.Transports =
+					HttpTransportType.WebSockets |
+					HttpTransportType.LongPolling;
+				options.CloseOnAuthenticationExpiration = true;
+				options.ApplicationMaxBufferSize = 65_536;
+				options.TransportMaxBufferSize = 65_536;
+				options.MinimumProtocolVersion = 0;
+				options.TransportSendTimeout = TimeSpan.FromSeconds(10);
+				options.WebSockets.CloseTimeout = TimeSpan.FromSeconds(3);
+				options.LongPolling.PollTimeout = TimeSpan.FromSeconds(10);
+			});
+		});
+		// app.UseEndpoints(endpoints => { endpoints.MapHub<THub>("/hub"); });
 
 		return app;
 	}
