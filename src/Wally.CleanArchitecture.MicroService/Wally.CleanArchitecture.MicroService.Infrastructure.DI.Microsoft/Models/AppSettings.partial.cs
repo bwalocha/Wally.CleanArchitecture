2a3b02a5446165644c5090c6fe1775e6a61b9c0b
wally <b.walocha@gmail.com>
2026-05-02 20:47:23 +0200

    up

---
+++ src/Wally.CleanArchitecture.MicroService/Wally.CleanArchitecture.MicroService.Infrastructure.DI.Microsoft/Models/AppSettings.partial.cs
@@ -0,0 +1,8 @@
+﻿namespace Wally.CleanArchitecture.MicroService.Infrastructure.DI.Microsoft.Models;
+
+// TODO: Remove Setters,
+// extract interfaces
+// and add ConventionTests
+public sealed partial class AppSettings
+{
+}
