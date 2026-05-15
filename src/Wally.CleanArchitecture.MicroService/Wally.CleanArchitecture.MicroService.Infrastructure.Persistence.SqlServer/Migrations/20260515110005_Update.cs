6f24c6062bed6090f713b175715d3768a998ec77
wally <b.walocha@gmail.com>
2026-05-15 13:52:09 +0200

    Up

---
+++ src/Wally.CleanArchitecture.MicroService/Wally.CleanArchitecture.MicroService.Infrastructure.Persistence.SqlServer/Migrations/20260515110005_Update.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wally.CleanArchitecture.MicroService.Infrastructure.Persistence.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSystemPaused",
+                schema: "MicroService",
+                table: "CronTickers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSystemPaused",
+                schema: "MicroService",
+                table: "CronTickers");
+        }
+    }
+}
--- src/dotnet-tools-update.cmd
+++ src/dotnet-tools-update.cmd
@@ -1,2 +1,3 @@
+SET  DOTNET_CLI_TELEMETRY_OPTOUT=1
 dotnet tool update --all --local --framework net10.0
 pause
