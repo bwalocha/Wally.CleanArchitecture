058768652be2a2bf2854e7382aea3047cf7fd17a
wally <b.walocha@gmail.com>
2026-07-18 20:43:29 +0200

    up

--- src/Wally.CleanArchitecture.MicroService-nuget-update.cmd
+++ src/Wally.CleanArchitecture.MicroService-nuget-update.cmd
@@ -1,2 +1,2 @@
-dotnet outdated --upgrade:Prompt --recursive --exclude MassTransit --older-than 3 --idle-timeout 120 --fail-on-updates .\Wally.CleanArchitecture.MicroService.sln
+dotnet outdated --upgrade:Prompt --recursive --exclude MassTransit --older-than 7 --idle-timeout 120 --fail-on-updates .\Wally.CleanArchitecture.MicroService.sln
 pause
